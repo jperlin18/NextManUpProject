e1fac11bfa4d83b7e2bb68b1a1d95f5e5ee113c2
Jeff Bailey <bailey27@illinois.edu>
2014-11-16 17:45:29 -0800

    Action bar taller, blue, white bold text

--- nextmanup/NextManUp/gen/com/devingotaswitch/nextmanup/R.java
+++ nextmanup/NextManUp/gen/com/devingotaswitch/nextmanup/R.java
@@ -64,5 +64,7 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int MyActionBar=0x7f060002;
+        public static final int MyTheme_ActionBar_TitleTextStyle=0x7f060003;
     }
 }
