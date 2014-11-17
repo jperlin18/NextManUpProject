601256715c04f9d2d2d8b920ce6654a31e9b2ac5
Jeff Bailey <bailey27@illinois.edu>
2014-11-16 17:47:54 -0800

    Add side menu lib, remove menu options

--- nextmanup/NextManUp/gen/com/devingotaswitch/nextmanup/R.java
+++ nextmanup/NextManUp/gen/com/devingotaswitch/nextmanup/R.java
@@ -8,7 +8,21 @@
 package com.devingotaswitch.nextmanup;
 
 public final class R {
+    public static final class anim {
+        public static final int side_navigation_fade_in=0x7f040000;
+        public static final int side_navigation_fade_out=0x7f040001;
+        public static final int side_navigation_in_from_left=0x7f040002;
+        public static final int side_navigation_in_from_right=0x7f040003;
+        public static final int side_navigation_out_to_left=0x7f040004;
+        public static final int side_navigation_out_to_right=0x7f040005;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int side_navigation_background=0x7f050000;
+        public static final int side_navigation_item_text_color=0x7f050003;
+        public static final int side_navigation_list_divider_color=0x7f050002;
+        public static final int side_navigation_outside_background=0x7f050001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,25 +31,43 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060008;
+        public static final int activity_vertical_margin=0x7f060009;
+        public static final int side_navigation_item_image_height=0x7f060003;
+        public static final int side_navigation_item_image_padding=0x7f060005;
+        public static final int side_navigation_item_image_width=0x7f060004;
+        public static final int side_navigation_item_padding_leftright=0x7f060002;
+        public static final int side_navigation_item_padding_topbottom=0x7f060001;
+        public static final int side_navigation_item_text_padding_left=0x7f060007;
+        public static final int side_navigation_item_text_size=0x7f060006;
+        public static final int side_navigation_width=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int side_navigation_shadow_left=0x7f020001;
+        public static final int side_navigation_shadow_right=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int shadow=0x7f0a0005;
+        public static final int side_navigation_item_icon=0x7f0a0000;
+        public static final int side_navigation_item_text=0x7f0a0001;
+        public static final int side_navigation_listview=0x7f0a0004;
+        public static final int side_navigation_menu=0x7f0a0003;
+        public static final int side_navigation_outside_view=0x7f0a0002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int side_navigation_item=0x7f030001;
+        public static final int side_navigation_left=0x7f030002;
+        public static final int side_navigation_right=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int hello_world=0x7f070002;
     }
     public static final class style {
         /** 
@@ -59,12 +91,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int MyActionBar=0x7f060002;
-        public static final int MyTheme_ActionBar_TitleTextStyle=0x7f060003;
+        public static final int AppTheme=0x7f080001;
+        public static final int MyActionBar=0x7f080002;
+        public static final int MyTheme_ActionBar_TitleTextStyle=0x7f080003;
     }
 }
