69a3c6b34d5f0c75af58ae2153519cdad5bb2833
Jeff Bailey <bailey27@illinois.edu>
2014-12-07 12:17:44 -0800

    Some filler to player class

--- nextmanup/NextManUp/src/com/devingotaswitch/nextmanup/specifics/Player.java
+++ nextmanup/NextManUp/src/com/devingotaswitch/nextmanup/specifics/Player.java
@@ -1,5 +1,35 @@
 package com.devingotaswitch.nextmanup.specifics;
 
 public class Player {
+	
+	private final String name;
+	private final String team;
+	private final String position;
+	
+	/**
+	 * Not entirely sure if this will be necessary, 
+	 * but it could be internally
+	 */
+	public Player() {
+		name = "unknown";
+		team = "unknown";
+		position = "unknown";
+	}
 
+	public Player(String name, String team, String position) {
+		this.name = name;
+		this.team = team;
+		this.position = position;
+	}
+	
+	/**
+	 * Player name with team appended is a unique way to 
+	 * identify a player (i.e. there were two Steve Smith's 
+	 * who played WR recently)
+	 * 
+	 * @return name.team
+	 */
+	public String getIdentifier(){
+		return name + "." + team;
+	}
 }
